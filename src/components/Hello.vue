a023666c74979bf09e46d54d6cfb1d1cf927f606
silasedubjg <silas.edu.bjg@gmail.com>
2021-06-13 16:42:39 -0300

    inicio de cprojeto para um crud em vue.js

---
+++ src/components/Hello.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="hello">
+    <md-toolbar>
+      <md-button class="md-icon-button">
+        <md-icon>menu</md-icon>
+      </md-button>
+      <h2 class="md-title" style="flex: 1">Default</h2>
+      <md-button class="md-icon-button">
+        <md-icon>favorite</md-icon>
+      </md-button>
+    </md-toolbar>
+    <md-tabs md-centered>
+      <md-tab md-label="Movies" md-icon="ondemand_video">
+        <div class="field-group">
+          <md-input-container>
+            <label for="movie">Movie</label>
+            <md-select name="movie" id="movie" v-model="movie">
+              <md-option value="fight_club">Fight Club</md-option>
+              <md-option value="godfather">Godfather</md-option>
+              <md-option value="godfather_ii">Godfather II</md-option>
+              <md-option value="godfather_iii">Godfather III</md-option>
+              <md-option value="godfellas">Godfellas</md-option>
+              <md-option value="pulp_fiction">Pulp Fiction</md-option>
+              <md-option value="scarface">Scarface</md-option>
+            </md-select>
+          </md-input-container>
+        </div>
+        <md-card style="width: 320px" class="card-ripple" md-with-hover>
+          <md-card-media>
+            <md-ink-ripple></md-ink-ripple>
+            <img
+              src="https://vuematerial.github.io/assets/card-image-1.jpg"
+              alt="People"
+            />
+          </md-card-media>
+          <md-card-actions>
+            <md-button class="md-icon-button">
+              <md-icon>favorite</md-icon>
+            </md-button>
+            <md-button class="md-icon-button">
+              <md-icon>bookmark</md-icon>
+            </md-button>
+            <md-button class="md-icon-button">
+              <md-icon>share</md-icon>
+            </md-button>
+          </md-card-actions>
+        </md-card>
+      </md-tab>
+      <md-tab md-label="Music" md-icon="music_note">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt
+          dolorum quas amet cum vitae, omnis! Illum quas voluptatem, expedita
+          iste, dicta ipsum ea veniam dolore in, quod saepe reiciendis nihil.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt
+          dolorum quas amet cum vitae, omnis! Illum quas voluptatem, expedita
+          iste, dicta ipsum ea veniam dolore in, quod saepe reiciendis nihil.
+        </p>
+      </md-tab>
+      <md-tab md-label="Books" md-icon="books">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt
+          dolorum quas.
+        </p>
+      </md-tab>
+      <md-tab md-label="Pictures" md-icon="photo">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt
+          dolorum quas.
+        </p>
+      </md-tab>
+    </md-tabs>
+  </div>
+</template>
